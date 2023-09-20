5ce0f777be8121e4209027bbb87719fcb057051e
Dawid Stasiak <dawid.stasiak.dc@gmail.com>
2023-09-20 21:51:31 +0200

    better db errors handling + data validation

--- api/bookController.go
+++ api/bookController.go
@@ -4,6 +4,7 @@ import (
 	"books/model"
 	"github.com/jinzhu/gorm"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,20 +14,35 @@ func addBook(c *gin.Context) {
 
 	var input model.Book
 	if err := c.ShouldBindJSON(&input); err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	db.Create(input)
+	if !isAuthorValid(input.Author) {
+		c.JSON(http.StatusBadRequest, "The authors name has to start with letter A")
+		return
+	}
 
-	c.IndentedJSON(http.StatusOK, input)
+	if err := db.Create(input).Error; err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, input)
 }
 
 func getAllBooks(c *gin.Context) {
 	db := c.MustGet(dbKey).(*gorm.DB)
 
 	var books []model.Book
-	db.Find(&books)
+	if err := db.Find(&books).Error; err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
 
-	c.IndentedJSON(http.StatusOK, books)
+	c.JSON(http.StatusOK, books)
+}
+
+func isAuthorValid(author string) bool {
+	return strings.HasPrefix(strings.ToLower(author), "a")
 }
